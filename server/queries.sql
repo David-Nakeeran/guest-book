93741e45c6eb7c478954dd6eaf23ded997886a43
David Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-05-22 14:15:03 +0100

    Created sql table

---
+++ server/queries.sql
@@ -0,0 +1,8 @@
+-- Table
+CREATE TABLE messages (
+    id bigint generated always as identity primary key,
+    first_name VARCHAR(255),
+    surname VARCHAR(255),
+    message TEXT,
+    created_at TIMESTAMP DEFAULT NOW()
+);
