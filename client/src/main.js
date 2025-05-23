a57d54841ebf900dc4cf9f4a1489a15a7b806879
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-05-23 10:57:46 +0100

    Added form handler

--- client/src/main.js
+++ client/src/main.js
@@ -1,1 +1,23 @@
-console.log("Hello World!");
+const form = document.querySelector("form");
+
+const submitHandler = async (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+
+  const formValues = Object.fromEntries(formData);
+
+  const response = await fetch("http://localhost:8080", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formValues),
+  });
+  const result = await response.json();
+  if (result.success) {
+    form.reset();
+  }
+};
+
+form.addEventListener("submit", submitHandler);
