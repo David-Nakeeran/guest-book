353a4186c229432bcea883c7b5bd7d4d98e8e79b
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-05-24 21:09:46 +0100

    Updated

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,7 @@
 const form = document.querySelector("form");
 const commentsContainer = document.getElementById("comments-container");
 const likes = document.querySelectorAll("[data-like]");
+const messagesBtn = document.getElementById("messages-btn");
 
 const submitHandler = async (e) => {
   e.preventDefault();
@@ -29,17 +30,24 @@ const createElements = (arr) => {
   arr.forEach((element, index) => {
     const messageContainer = document.createElement("div");
     messageContainer.setAttribute("data-index", index);
+    messageContainer.classList.add("message");
     commentsContainer.append(messageContainer);
 
     const firstNamePara = document.createElement("p");
     const surnamePara = document.createElement("p");
     const messagePara = document.createElement("p");
-    const likesPara = document.createElement("p");
+    const likesContainer = document.createElement("div");
+    const likesCounterPara = document.createElement("p");
+    const likesTextPara = document.createElement("p");
+    const btnContainer = document.createElement("div");
+
+    btnContainer.classList.add("button-container");
 
     firstNamePara.textContent = `First name: ${element.first_name}`;
     surnamePara.textContent = `Surname: ${element.surname}`;
     messagePara.textContent = `Message: ${element.message}`;
-    likesPara.textContent = `Likes: ${element.likes || 0}`;
+    likesTextPara.textContent = "Likes: ";
+    likesCounterPara.textContent = parseInt(element.likes) || 0;
 
     const likeBtn = document.createElement("button");
     likeBtn.setAttribute("data-like", element.id);
@@ -53,13 +61,24 @@ const createElements = (arr) => {
       deleteBtn.parentElement.remove();
     });
 
+    likeBtn.addEventListener("click", async () => {
+      const result = await updateLikeMessageData(element.id);
+      if (!result.success) {
+        return;
+      }
+      likesCounterPara.textContent++;
+    });
+
+    likesContainer.classList.add("likes-container");
+
+    btnContainer.append(deleteBtn, likeBtn);
+    likesContainer.append(likesTextPara, likesCounterPara);
     messageContainer.append(
       firstNamePara,
       surnamePara,
       messagePara,
-      likesPara,
-      deleteBtn,
-      likeBtn
+      likesContainer,
+      btnContainer
     );
   });
 };
@@ -85,19 +104,32 @@ const deleteData = async (id) => {
   }
 };
 
+const updateLikeMessageData = async (id) => {
+  try {
+    const response = await fetch(`http://localhost:8080/${id}/likes`, {
+      method: "PUT",
+    });
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 const coordinator = async () => {
   commentsContainer.innerHTML = "";
   const data = await getData();
   createElements(data);
 };
 
-// Event listener for like
-likes.forEach((like) => {
-  like.addEventListener("click", (e) => {
-    console.log(e.target.id);
-  });
-});
+const messageHandler = () => {
+  commentsContainer.classList.toggle("hidden");
+  if (messagesBtn.textContent === "Show Messages") {
+    messagesBtn.textContent = "Hide Messages";
+  } else {
+    messagesBtn.textContent = "Show Messages";
+  }
+};
+
+messagesBtn.addEventListener("click", messageHandler);
 
 coordinator();
-
-// Get icon for likes
